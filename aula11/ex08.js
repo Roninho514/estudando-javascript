8ca650a189dab1aba5303756e619905ebee13cab
Roni dos Santos Silva <82835587+Roninho514@users.noreply.github.com>
2022-06-08 22:48:29 -0300

    exercícios feitos 08/06

---
+++ aula11/ex08.js
@@ -0,0 +1,6 @@
+var vel = 30.5
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if(vel > 60){
+   console.log('VocÊ ultrapassou a velocidade. Multado!')
+}
+console.log('Dirija usando sinto de segurança')
