873e2f24dc98405b0cf14595f38845adda194fec
Roni dos Santos Silva <82835587+Roninho514@users.noreply.github.com>
2024-08-13 19:30:17 -0300

    learning about components in react

---
+++ react/2_FUNDAMENTOS/2_Fundamentos/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import FirstComponent from './components/FirstComponent';
+// 2 - Importando componente
+function App() {
+ // 3 - Comentários
+  return (
+    <>
+      {/* 3 - Comentario dentro do jsx */}
+      <h2>Fundamentos React</h2>
+      <FirstComponent/>
+    </>
+  )
+}
+
+export default App
